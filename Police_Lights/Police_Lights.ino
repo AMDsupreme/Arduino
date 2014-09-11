5df32672bb7f3d48b5c06480631996e9b55e2a1a
AMDsupreme <chadkrause308@gmail.com>
2014-09-11 13:49:27 -0400

    Basic lights
    Commented out the PWM read (needs testing)
    
    Put on basic lights, used by the LightsOn() function.
    
    Tried to code for easy configuration
    
    Not ready for deploying on Arduino. Needs to have something fed into
    the if statement to activate the lights
    
    Compiles

--- Police_Lights/Police_Lights.ino
+++ Police_Lights/Police_Lights.ino
@@ -1,11 +1,46 @@
 #include <Adafruit_NeoPixel.h>
-const int pin = 1;
-
+const int pin = 1, PX = 2, numLEDs = 18, half = (numLEDs/2);
+int PXon;
+int OnValue = 0; //This is the variable to change for the PWM on/off trigger
 //18 LEDs
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(18, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numLEDs, pin, NEO_GRB + NEO_KHZ800);
 
 
 void setup(){
-  strip.begin()
-  strip.show()
+  strip.begin();
+  strip.show();
+  pinMode(pin, OUTPUT); //Output to NeoPixel
+  pinMode(PX, INPUT); //RC input from the Pixhawk
+  Serial.begin(9600);
+
 }
+
+void loop(){
+  //PXon = pulseIn(PX, HIGH); //read PWM signal from the
+  if(PXon > OnValue){
+  }
+  else{
+
+  }
+
+
+}
+
+
+void LightsOn(){
+  for(int i = 0; i < 5; i++){
+    for(int i = 0; i <= half; i++){
+      strip.setPixelColor(i, strip.Color(255,0,0));
+      strip.setPixelColor((i + half), strip.Color(0,0,255));
+    }
+    strip.show();
+    delay(50);
+    for(int i = 0; i <= half; i++){
+      strip.setPixelColor(i, strip.Color(0,0,255));
+      strip.setPixelColor((i + half), strip.Color(255,0,0));
+    }
+    strip.show();
+    delay(50);
+  }
+}
+
