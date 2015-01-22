cb128719e86a573caac402e28c92dcf904401b9f
AMDsupreme <chadkrause308@gmail.com>
2015-01-22 00:53:45 -0500

    Fixed analogRead() and LED issues
    The analogRead() was very noisy. Added a 1000uf Capacitor to help
    stabilize the read and it worked like a charm.
    
    The check to see if reading was equal to distSetting accidentally set
    reading to distSetting, so I fixed that.

--- Parking_Sensor_V2/Parking_Sensor_V2.ino
+++ Parking_Sensor_V2/Parking_Sensor_V2.ino
@@ -7,8 +7,11 @@ int ledCount = 30;
 int ledHalf = ledCount/2;
 int distSetting = 12; //default setting for the golden distance
 int distSettingPin = A0;
-int ledPin = 3;
+int ledPin = 12;
 int tPin = 4, ePin = 5;
+int distSettingAnalogValue = 0;
+//delete these variables
+int checktimes = 0;
 
 NewPing sonar(tPin, ePin, 305); // 306 = approx. 10 feet. For timeout reasons.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(ledCount, ledPin, NEO_GRB + NEO_KHZ800);
@@ -19,8 +22,12 @@ uint32_t yellow = strip.Color(255,255,0);
 
 void setup(){
   Serial.begin(9600);
-  setDistanceSetting();
-
+  
+  pinMode(ledPin, OUTPUT);
+  strip.begin();
+  strip.setBrightness(20);
+  strip.show();
+  setDistanceSetting(analogRead(distSettingPin));
 }
 
 void loop(){
@@ -29,33 +36,49 @@ void loop(){
 }
 
 void checkSettings(){
-  int analogValue = analogRead(distSettingPin);
-  int potCheck = (analogValue/1024.0) * 48;
-  
-  if(abs(potCheck-distSetting) >= 1){
-    setDistanceSetting();
+  int analogValue1 = analogRead(distSettingPin);
+  int potCheck = (analogValue1/1024.0) * 48;
+  if(potCheck <= 3){ //check to see if the distance is less than 3, and if it is, change it to 3
+    potCheck = 3;
+  }
+
+  if(abs(analogValue1-distSettingAnalogValue) >= 10){
+    Serial.println(abs(analogValue1-distSettingAnalogValue));
+    //Serial.print("checkSetting\t");
+    //Serial.println(checktimes);   
+    checktimes++;// remember to delete this line
+    setDistanceSetting(analogValue1);
+
   }
 }
 
-void setDistanceSetting(){
-  int analogValue = analogRead(distSettingPin);
-  int distCheck = (analogValue/1024.0) * 48; //48 inches or 4 ft max distance (any further would be a small window)
+void setDistanceSetting(int analogv){
+
+  distSettingAnalogValue = analogv;
+  //Serial.print(analogValue);
+  //Serial.print("\t");
+  int distCheck = (analogv/1024.0) * 48; //48 inches or 4 ft max distance (any further would be a small window)
   if(distCheck <= 3){ //check to see if the distance is less than 3, and if it is, change it to 3
     distSetting = 3;
   }
   else{
     distSetting = distCheck;
   }
-  Serial.println(distSetting);
+  //Serial.println(distSetting);
   displayDistanceSetting();
 }
 
 void displayDistanceSetting(){
-  int reading = sonar.convert_in(sonar.ping_median(3));
+  //int reading = sonar.convert_in(sonar.ping_median(3));
+  int reading = 21;
   uint32_t setDistColor = yellow;
-
-  if(reading = distSetting){
-    setDistColor = green; 
+  int distSet = 22;
+  if(reading == distSetting){
+    setDistColor = green;
+    Serial.print("True\t");
+   Serial.print(distSet);
+   Serial.print("\t");
+   Serial.println(reading);
   }
 
   if(reading > ledCount){
@@ -63,20 +86,35 @@ void displayDistanceSetting(){
     for(int i = 0; i < ledCount; i++){
       strip.setPixelColor(i, setDistColor);
       strip.show();
-      delay(1);
+      delay(100);
     }
     for(int i = ledCount; i < leftover; i++){
       strip.setPixelColor(i, setDistColor);
       strip.show();
-      delay(1);
+      delay(100);
     }
   }
     else{
-      for(int i = 0; i < ledCount; i++){
+      for(int i = 0; i < distSetting; i++){
         strip.setPixelColor(i, setDistColor);
         strip.show();
-        delay(1);
-      }    
+        delay(3);
+        }
+      for(int i = distSetting; i < ledCount; i++){
+        strip.setPixelColor(i, 0);
+        strip.show();
+        delay(2);    
     }
   }
+}
+
+
+
+
+//Added Capacitor to help analogRead() noise
+//made the setting update check analog based not distance based
+//fixed LEDs
+//git
+//git1
+
 
